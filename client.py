17a9d1fd7cb5c0762aa3981bcd74270f1f46cb34
DEEPIKASINGH608 <ds0648775@gmail.com>
2026-07-01 22:09:13 +0530

    Final commit

--- client.py
+++ client.py
@@ -1,12 +1,15 @@
 from openai import OpenAI
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 client = OpenAI(
-        api_key="OPENAI_API_KEY"
+        api_key=os.getenv("OPENAI_API_KEY")
     )
 
 completion = client.chat.completions.create(
-        model="gpt-4.1-mini",
+        model="gpt-4o-mini",
         messages=[
             {"role": "system", "content": "You are a virtual assistant that named Jarvis skilled in tasks like Alexa and Google Cloud"},
             {"role": "user", "content": "What is coding?"}
